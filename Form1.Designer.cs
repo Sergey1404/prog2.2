4b8341e48716d84463640654579d973ce3763354
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2022-11-16 16:49:52 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(369, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(19, 116);
+            this.chart1.Name = "chart1";
+            series17.BorderWidth = 3;
+            series17.ChartArea = "ChartArea1";
+            series17.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series17.Color = System.Drawing.Color.Black;
+            series17.LabelBorderWidth = 2;
+            series17.Legend = "Legend1";
+            series17.Name = "Функция f(x)";
+            series17.YValuesPerPoint = 3;
+            series18.ChartArea = "ChartArea1";
+            series18.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series18.Color = System.Drawing.Color.Red;
+            series18.LabelBorderWidth = 2;
+            series18.LabelForeColor = System.Drawing.Color.Purple;
+            series18.Legend = "Legend1";
+            series18.Name = "Точка локального минимума";
+            series19.ChartArea = "ChartArea1";
+            series19.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series19.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series19.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series19.LabelForeColor = System.Drawing.Color.Bisque;
+            series19.Legend = "Legend1";
+            series19.Name = "Начальная граница";
+            series19.YValuesPerPoint = 4;
+            series20.ChartArea = "ChartArea1";
+            series20.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series20.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series20.Legend = "Legend1";
+            series20.Name = "Конечная граница";
+            this.chart1.Series.Add(series17);
+            this.chart1.Series.Add(series18);
+            this.chart1.Series.Add(series19);
+            this.chart1.Series.Add(series20);
+            this.chart1.Size = new System.Drawing.Size(769, 310);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(34, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "f(x) = ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(369, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(57, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(605, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(89, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(366, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Начальная граница";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(367, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Конечная граница";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "< X <";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(246, 26);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(40, 20);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(311, 26);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(40, 20);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(602, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Точка минимума";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(509, 29);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(61, 20);
+            this.textBox7.TabIndex = 13;
+            this.textBox7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(506, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Точность E";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Метод половинного деления";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using AngouriMath;
+using AngouriMath.Core.Exceptions;
+using AngouriMath.Extensions;
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        //Нельзя вводить буквы в textbox (кроме формулы, туда можно)
+        void TextBox1KeyPress(object sender, KeyPressEventArgs e) {
+            var number = e.KeyChar;
+            //   Число                   Запятая         Backspace      Минус
+            if (!Char.IsDigit(number) && number != 44 && number != 8 && number != 45) {
+                e.Handled = true;
+            }
+        }
+        //Для точности
+        void TextBox2KeyPress(object sender, KeyPressEventArgs e) {
+            var number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 44 && number != 8) {
+                e.Handled = true;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+                double lowerLimitX = Convert.ToDouble(textBox5.Text);
+                double upperLimitX = Convert.ToDouble(textBox6.Text);
+                double firstLimit = Convert.ToDouble(textBox2.Text);
+                double secondLimit = Convert.ToDouble(textBox3.Text);
+                if (lowerLimitX < upperLimitX) {
+                    if (firstLimit >= lowerLimitX && firstLimit <= upperLimitX && secondLimit >= lowerLimitX && secondLimit <= upperLimitX) {
+                        chart1.Series[0].Points.Clear();
+                        chart1.Series[1].Points.Clear();
+                        chart1.Series[2].Points.Clear();
+                        chart1.Series[3].Points.Clear();
+                        //Парсим формулу из textbox
+                        Entity formula = Convert.ToString(textBox1.Text);
+                        formula = formula.Simplify();
+                        //chart1.Series[0].XValueType = ChartValueType.Double;
+                        //chart1.ChartAreas[0].AxisX.IntervalOffsetType = DateTimeIntervalType.Number;
+                        double y = 0;
+                        //Строим график
+                        for (double i = lowerLimitX; i < upperLimitX + 1; i += 1) {
+                            //основной график
+                            y = (double)formula.Substitute("x", i).EvalNumerical();
+                            chart1.Series[0].Points.AddXY(i, y);
+                            //Начальная граница 
+                            var addx1 = Convert.ToDouble(textBox2.Text);
+                            chart1.Series[2].Points.AddXY(firstLimit, 0);
+                            chart1.Series[2].Points.AddXY(firstLimit, y);
+                            //Конечная граница 
+                            var addx2 = Convert.ToDouble(textBox3.Text);
+                            chart1.Series[3].Points.AddXY(addx2, 0);
+                            chart1.Series[3].Points.AddXY(addx2, y);
+                        }
+                        double Fx = 0;
+                        double x = 0;
+                        ////Функция от границ, для условия сходимости
+                        //var convA = (double)formula.Substitute("x", firstLimit).EvalNumerical();
+                        //var convB = (double)formula.Substitute("x", secondLimit).EvalNumerical();
+                        //Точность
+                        double precision = Convert.ToDouble(textBox7.Text);
+                        //Сигма для формул
+                        double sigma = precision / 2 - precision / 4;
+                        do {
+                            chart1.Series[1].Points.Clear();
+                            x = (firstLimit + secondLimit) / 2;
+                            double l = x - sigma;
+                            double r = x + sigma;
+                            double Fl = (double)formula.Substitute("x", l).EvalNumerical();
+                            double Fr = (double)formula.Substitute("x", r).EvalNumerical();
+                            Fx = (double)formula.Substitute("x", x).EvalNumerical();
+                            if (Fl <= Fr){
+                                secondLimit = r;
+                            }
+                            else if (Fl > Fr) {
+                                firstLimit = l;
+                            }
+                            chart1.Series[1].Points.AddXY(x, Fx);
+                            textBox4.Text = Convert.ToString(Math.Round(x, 6));
+                        } while (Math.Abs(firstLimit - secondLimit) >= precision);
+                    }
+                    else {
+                        chart1.Series[0].Points.Clear();
+                        chart1.Series[1].Points.Clear();
+                        chart1.Series[2].Points.Clear();
+                        chart1.Series[3].Points.Clear();
+                        MessageBox.Show("Границы поиска выходят за границы X");
+                    }
+                }
+                else {
+                    MessageBox.Show("Неверные границы X", "Ошибка");
+                }
+            }
+            catch (AngouriMathBaseException) {
+                MessageBox.Show("Ошибка в формуле или введенных границах X", "Ошибка");
+            }
+            catch (FormatException) {
+                MessageBox.Show("Ошибка формата введенных данных", "Ошибка");
+            }
+            catch (Exception) {
+                MessageBox.Show("Ошибка: {ex.Source}", "Ошибка");
+            }
+        }
+    }
+}
